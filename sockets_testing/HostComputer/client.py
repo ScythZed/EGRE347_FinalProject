f3b07e47132392cd78b542d555e1792cdfb07fb9
Samuel Graves <96082253+ScythZed@users.noreply.github.com>
2022-12-08 17:43:15 -0500

    Add files via upload

--- sockets_testing/HostComputer/client.py
+++ sockets_testing/HostComputer/client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 HOST = '192.168.1.23'
 PORT = 12345
@@ -7,9 +8,35 @@ s.connect((HOST,PORT))
 
 
 while True:
-    command = bytes(input('Enter your command: '), 'utf-8')
+    # Menu Print
+    print("Enter option:")
+    print("		(1) Print the part list")
+    print("		(2) Print a specific part number")
+    print("		(3) Add a part to the list")
+    print("		(4) Sort the list by part number")
+    print("		(5) Save the list")
+    print("		(6) Exit the program")
+
+    # Intakes command and sends to server
+    command = bytes(input("				Choice ?"), 'utf-8')
     s.send(command)
-    reply = s.recv(1024)
-    if reply == bytes('Terminating','utf-8'):
+
+    # Menu 1 (Working, but a bit finicky. Sometimes only prints out some of the data.)
+    if(command.decode == '1'):
+        Looping = True  # Bool for looping until all are printed
+        while Looping:
+            time.sleep(1)   # added a time delay to see if it will fix^
+            reply = s.recv(1024).decode()
+            print(reply)
+            if(reply == "done"):
+                s.send(bytes("done",'utf-8'))
+                Looping = False
+        
         break
-    print(reply)
+
+
+
+    reply = s.recv(1024)
+    if reply.decode() == 'Terminating':
+        break
+    print(reply.decode())
