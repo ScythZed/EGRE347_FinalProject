856d6cf4654ee442e8d1c2022c283527162ef3ca
ScythZed <scythzhd@gmail.com>
2022-12-12 20:31:47 -0500

    Comments

--- sockets_testing/Pi/server.py
+++ sockets_testing/Pi/server.py
@@ -20,7 +20,6 @@ print("Server awaiting messages")
 conn, addr = s.accept()
 print("Connected")
 
-################## Class testing/data ##################
 # Variable
 infile = "infile2.part" # File name
 parts_list = [] # list of parts
@@ -47,26 +46,28 @@ while(loop):
         break
     parts_list.append(temp)
     count += 1
-########################################################
 
-while True:
-    data = conn.recv(4096)
+
+while True: # Server Menu Loop
+    data = conn.recv(4096)  # Recives message at the start of every loop. This message controls the response
     print("I sent a message in response to: ", data.decode())
 
     temp_loop = True
     # Process Data
     if data.decode() == '1':    # Print the part list
         attempt = ""
-        for i in range(len(parts_list)):
+        for i in range(len(parts_list)):    # Loops through the part list, adds it to a string, and then sends the data
             attempt += "--------------------------------------------------------------------------------\n"
             attempt += str(parts_list[i].part_number) + " " + str(parts_list[i].part_name) + "\n"
             attempt += str(parts_list[i].description) + "\n"
 
+            # family type conversion
             if (parts_list[i].family == 0): attempt += "TTL" + "\n"
             elif (parts_list[i].family == 1): attempt += "BiCMOS" + "\n"
             elif (parts_list[i].family == 2): attempt += "CMOS" + "\n"
             else: print("ERROR ",end="")
 
+            # package type conversion
             if (parts_list[i].package == 0): attempt += "SSOP"  + "\n"
             elif (parts_list[i].package == 1): attempt += "SOIC " + "\n"
             elif (parts_list[i].package == 2): attempt += "DIP" + "\n"
@@ -75,6 +76,7 @@ while True:
             elif (parts_list[i].package == 5): attempt += "SO" + "\n"
             else: print("ERROR")
 
+            # Vcc conversion
             for i in range(4):
                 if(parts_list[i].vcc[i] != 0.0):
                     attempt += str(parts_list[i].vcc[i]) + "V"
@@ -95,17 +97,19 @@ while True:
         print(search)
         found = False
         attempt = ""
-        for i in range(len(parts_list)):
+        for i in range(len(parts_list)):    # searches for a part and adds it to the list
             if(parts_list[i].getPartNumber() == search):
                 attempt += "\n--------------------------------------------------------------------------------\n"
                 attempt += str(parts_list[i].part_number) + " " + str(parts_list[i].part_name) + "\n"
                 attempt += str(parts_list[i].description) + "\n"
 
+                 # family type conversion
                 if (parts_list[i].family == 0): attempt += "TTL" + "\n"
                 elif (parts_list[i].family == 1): attempt += "BiCMOS" + "\n"
                 elif (parts_list[i].family == 2): attempt += "CMOS" + "\n"
                 else: print("ERROR ",end="")
 
+                # package type conversion
                 if (parts_list[i].package == 0): attempt += "SSOP"  + "\n"
                 elif (parts_list[i].package == 1): attempt += "SOIC " + "\n"
                 elif (parts_list[i].package == 2): attempt += "DIP" + "\n"
@@ -114,6 +118,7 @@ while True:
                 elif (parts_list[i].package == 5): attempt += "SO" + "\n"
                 else: print("ERROR")
 
+                # Vcc conversion
                 for i in range(4):
                     if(parts_list[i].vcc[i] != 0.0):
                         attempt += str(parts_list[i].vcc[i]) + "V"
@@ -136,9 +141,11 @@ while True:
             conn.send(bytes(reply,'utf-8'))
             continue
     elif data.decode() == '3':  # Add a part to the list
-        new_part = ""
-        while(new_part == ""):
+        new_part = "" # Temp string to recive data
+        while(new_part == ""): # Waits to recive the data
             new_part = conn.recv(1024).decode()
+
+        # Adds data into temp object
         part = SNx4xx.SNx4xx()
         new_part = new_part.split("\n")
         part.setPartNumber(new_part[0])
@@ -147,22 +154,22 @@ while True:
         part.setFamily(new_part[3])
         part.setPackage(new_part[4])
         part.setVcc(new_part[5])
-        parts_list.append(part)
+        parts_list.append(part) # Adds to the parts list
         #reply = "Part added!"
     elif data.decode() == '4':  # Sort the list by part number
         parts_list.sort(key=lambda x: x.part_number)
         reply = "List is sorted!"
         conn.send(bytes(reply,'utf-8'))
-    elif data.decode() == '5':  # Sort the list by part number
-        file_name = "outfile.part"
-        write_object = open(file_name, 'w')
-        for obj in parts_list:
+    elif data.decode() == '5':  # Save the list
+        file_name = "outfile.part" # outfile name
+        write_object = open(file_name, 'w') # opens file
+        for obj in parts_list: # loops and writes all object data into file
             obj.write(write_object)
         write_object.close()
         print("List is saved in " + file_name)
         reply = "List is saved to server!"
         conn.send(bytes(reply,'utf-8'))
-    elif data.decode() == "6":
+    elif data.decode() == "6": # Quits
         conn.send(bytes('Terminating','utf-8'))
         break
     else:
